97afe615da5016aa8e45970fca835834a6839d6f
SIB61 <sib.sustswe@gmail.com>
2022-11-27 21:21:16 +0600

    practice project done

--- ui/src/app/modules/profile/profile.module.ts
+++ ui/src/app/modules/profile/profile.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ProfileToolbarComponent } from './components/profile-toolbar/profile-toolbar.component';
 import { MateialModule } from '@common/mateial/mateial.module';
 import { ProfileService } from './services/profile.service';
-
 @NgModule({
   declarations: [ProfileToolbarComponent],
   imports: [CommonModule, MateialModule],
